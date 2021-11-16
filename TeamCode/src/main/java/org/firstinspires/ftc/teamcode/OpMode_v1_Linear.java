2b5ffd568c9e9ea5a72ec7bb2e3aa3595b12ac75
Stanley <heysian123popnley@gmail.com>
2021-11-16 16:55:57 -0500

    bug introduced: nothing works
    bug introduced: it sort of runs on its own forward after a bit
    
    bug introduced: the code intended to work didn't work.
    oops!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_v1_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_v1_Linear.java
@@ -30,13 +30,14 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="v.1.11: Linear OpMode", group="Linear Opmode")
+@TeleOp(name="v.1.12: Linear OpMode", group="Linear Opmode")
 public class OpMode_v1_Linear extends LinearOpMode {
 
     // Declare OpMode members.
@@ -45,7 +46,7 @@ public class OpMode_v1_Linear extends LinearOpMode {
     private DcMotor frontRight = null;
     private DcMotor backRight = null;
     private DcMotor backLeft = null;
-
+    private DcMotor scoopMotor = null;
 
     @Override
     public void runOpMode() {
@@ -59,6 +60,7 @@ public class OpMode_v1_Linear extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotor.class, "frontRightMotor");
         backLeft = hardwareMap.get(DcMotor.class, "backLeftMotor");
         backRight = hardwareMap.get(DcMotor.class, "backRightMotor");
+        scoopMotor = hardwareMap.get(DcMotor.class, "scoopMotor");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
@@ -66,6 +68,7 @@ public class OpMode_v1_Linear extends LinearOpMode {
         backLeft.setDirection(DcMotor.Direction.FORWARD);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
+        scoopMotor.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -77,6 +80,7 @@ public class OpMode_v1_Linear extends LinearOpMode {
             // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
             double rightPower;
+            double scoopPower = 0;
 
             // Choose to drive using either Tank Mode, or POV Mode
             // Comment out the method that's not used.  The default below is POV.
@@ -93,15 +97,34 @@ public class OpMode_v1_Linear extends LinearOpMode {
             // leftPower  = -gamepad1.left_stick_y ;
             // rightPower = -gamepad1.right_stick_y ;
 
-            // Send calculated power to wheels
+            // Scoop power/direction
+            while(gamepad1.y) {
+                if(gamepad1.x) {
+
+                } else {
+                    scoopPower = 0.5;
+                }
+            }
+            while (gamepad1.x) {
+                if (gamepad1.y) {
+
+                } else {
+                    scoopPower = -0.5;
+                }
+            }
+            while (!gamepad1.x && !gamepad1.y) {
+                scoopPower = 0;
+            }
+            // Send calculated power' to wheels
             frontLeft.setPower(leftPower);
             backLeft.setPower(leftPower);
             frontRight.setPower(rightPower);
             backRight.setPower(rightPower);
+            scoopMotor.setPower(scoopPower);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower, scoopPower);
             telemetry.update();
         }
     }
